80879a2defd21f651e9c760a406bd25d6832f3d2
Elvir <elvir.1998@hotmail.com>
2022-01-17 10:08:41 +0100

    fixed Index html

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = "5121042174eccb4974405dfa4c4c00bc1337e8ec7638e5d1b9d151c07845ef0e"
+
     return app
+
+
 
 if __name__ == '__main__':
     create_app().run()
