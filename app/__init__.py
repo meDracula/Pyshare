a7b57781d8efa1701cf72c53f6bfbd4d824024dd
Elvir <elvir.1998@hotmail.com>
2022-01-18 23:12:34 +0100

    added render_template +auth

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,12 @@ from app.persistence.db import init_db
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
+
     init_db(app)
+    from .auth import auth
+
+    app.register_blueprint(auth, url_prefix="/")
+
     return app
 
 
