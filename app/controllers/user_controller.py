e13bdfcf33cb04550595ddd5d69332158351b14a
meDracula <me.dracula@protonmail.com>
2022-01-19 16:52:06 +0100

    Verified user boolean logic
    The controller now offers the capablilty to verify if a user is in
    the database or not.
    
    Also search perameters have been changed to return now a list
    instead of a dict. This is throw utilizing the ResultList class.
    * function get_user renamed to get_users and will return mutiple users
      fitting the parameters inputed to the function.

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -4,14 +4,14 @@ from app.persistence.repository import user_repo
 def get_all_users():
     return user_repo.get_all_users()
 
-
-def get_user(username, email):
-    return user_repo.get_user(username, email)
-
+def get_users(username, email):
+    return user_repo.get_users(username, email)
 
 def create_new_user(username:str, email:str, password:str):
     return user_repo.create_new_user(username, email, password)
 
+def verify_user(username: str, email: str) -> bool:
+    return user_repo.verify_user(username, email)
 
 def verify_password(password_hash:str, password:str):
     return user_repo.verify_password(password_hash, password)
--- app/persistence/repository/user_repo.py
+++ app/persistence/repository/user_repo.py
@@ -5,12 +5,18 @@ def get_all_users():
     return User.all()
 
 
-def get_user(username, email):
-    return next(User.find(username=username, email=email))
+def get_users(username, email):
+    return User.find(username=username, email=email)
 
 
 def set_password(user, password:str):
     user.password = password
+
+
+def verify_user(username, email):
+    if User.find(username=username, email=email).first_or_none() is not None:
+        return True
+    return False
 
 
 def create_new_user(username:str, email:str, password:str):
