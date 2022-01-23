502daed81a873643970bf661ef61aa40d0afec8a
meDracula <me.dracula@protonmail.com>
2022-01-23 20:46:36 +0100

    Full backend support to users in frontend
    This commit is small but menat to give the frontend signup issue
    complete support it needs to finish.

--- docker/mongo/scripts/mongo-init.js
+++ docker/mongo/scripts/mongo-init.js
@@ -46,5 +46,5 @@ db.createUser({
 	}
 );
 
-db.status.insertOne({ status: 'confirmed' });
-
+db.users.createIndex({ "username": 1 }, { unique: true } )
+db.users.createIndex({ "email": 1 }, { unique: true } )
