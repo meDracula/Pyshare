42ec7e4f07cfea327043af9286fff119c46b9d96
Elvir <elvir.1998@hotmail.com>
2022-01-28 18:31:09 +0100

    added bp_user logout

--- app/blueprints/users/__init__.py
+++ app/blueprints/users/__init__.py
@@ -1,3 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import logout_user
 
 bp_user = Blueprint('bp_user', __name__)
+
+@bp_user.get('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('bp_open.home'))
+
