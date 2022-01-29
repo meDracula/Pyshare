ef01341cda6d9dab280f7f580b1d9fdfac1e075f
Elvir <elvir.1998@hotmail.com>
2022-01-29 16:41:29 +0100

    added flash tom logout

--- app/blueprints/users/__init__.py
+++ app/blueprints/users/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import logout_user, login_required
 
 bp_user = Blueprint('bp_user', __name__)
@@ -7,6 +7,7 @@ bp_user = Blueprint('bp_user', __name__)
 @bp_user.get('/logout')
 def logout():
     logout_user()
+    flash('You are now logged out')
     return redirect(url_for('bp_open.home'))
 
 
