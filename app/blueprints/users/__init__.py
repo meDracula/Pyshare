8de1711a7c12f783e903808e4c1664d634720649
meDracula <me.dracula@protonmail.com>
2022-01-31 15:12:29 +0100

    Solve problem page #31 Warning
    This code work and does as expected but is bad and bug written as fu*k
    take caution and understand that this code will be worked on and
    requires some future knowledge to be able write it to a acceptable
    state.

--- app/blueprints/users/__init__.py
+++ app/blueprints/users/__init__.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import logout_user, login_required, current_user
 
 bp_user = Blueprint('bp_user', __name__)
 
+color = False
+solve_text = ""
 
 @bp_user.get('/logout')
 def logout():
@@ -16,15 +18,35 @@ def logout():
 def account():
     return render_template('account.html')
 
-@bp_user.get('/<title>/solve')
+@bp_user.get('/posts/<title>/solve')
 @login_required
 def solve_thepost(title):
-    from app.controllers.post_controller import post_solution, get_post
+    from app.controllers.post_controller import get_post
     post = get_post(title)
-    solution_code = """
-                    def func():
-                        return 'hello world!'
-                    """
-    post_solution(current_user.username, solution_code, post)
-    return redirect(url_for("bp_open.thepost_get", title=title))
+    return render_template('solve.html', post=post, solve_text=solve_text, color=color)
+
+
+@bp_user.post('/posts/<title>/solve')
+@login_required
+def solve_thepost_post(title):
+    from app.controllers.post_controller import post_solution, get_post
+    from app.controllers.code_controller import test_it
+    post = get_post(title)
+
+    global color
+    user_text = request.form.get('solve_text')
+    submit = request.form.get('final')
+
+    color = test_it(post.test_code, user_text)
+
+    if color:
+        global solve_text
+        solve_text = user_text
+    else:
+        flash("Your code is RED!!!")
+
+    if submit:
+        post_solution(current_user.username, solve_text, post)
+        return redirect(url_for('bp_open.thepost_get', title=title))
+    return redirect(url_for('bp_user.solve_thepost', title=title))
 
