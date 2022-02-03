9c5edfe92947ce64a3d3963180e2fc0dee1c34ab
Elvir <elvir.1998@hotmail.com>
2022-02-03 11:53:30 +0100

    added admin page
    admins will now have access to the admin page
    -bp_admin.get added
    -admin.html added
    -elif statement in base.html added

--- app/blueprints/admin/__init__.py
+++ app/blueprints/admin/__init__.py
@@ -1,3 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for
-
+from flask_login import login_required
 bp_admin = Blueprint('bp_admin', __name__)
+
+@bp_admin.get('/admin')
+@login_required
+def admin_page():
+    return render_template('admin.html')
+
