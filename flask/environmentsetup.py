f2d52064284dc7b972be987fa6154ce57d6f1102
meDracula <me.dracula@protonmail.com>
2022-02-07 15:59:01 +0100

    Improving the secret key generator

--- flask/environmentsetup.py
+++ flask/environmentsetup.py
@@ -7,10 +7,10 @@ key = 'SECRET_KEY='+key+"\n"
 
 with open(env, 'r', encoding='UTF-8') as rf:
     data = rf.readlines()
-    for index, line in enumerate(data):
-        if "SECRET_KEY=" in line:
-            data[index] = key
+    if any('SECRET_KEY=' in line for line in data):
+        data = list(map(lambda x: key if 'SECRET_KEY=' in x else x, data))
+    else:
+        data.append(key)
 
     with open(env, 'w', encoding='UTF-8') as wf:
         wf.writelines(data)
-
