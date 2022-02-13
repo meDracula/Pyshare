46811d3df043dbef22ee8cf82d3fb549cb4f98d9
meDracula <me.dracula@protonmail.com>
2022-02-13 02:12:32 +0100

    Adding Session handling to Flask
    Removing and reducing global variables + the best part lower the amount
    of request a to get post from database

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,12 +1,16 @@
 from dotenv import load_dotenv
 from flask import Flask
 from flask_login import LoginManager
+from flask_session import Session
 from app.persistence.db import init_db
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
+    app.config["SESSION_PERMANENT"] = False
+    app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
 
     init_db(app)
 
